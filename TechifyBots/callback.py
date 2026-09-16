8721ce92370c1b859b8ccafae514cb68b7ce320a
Techify Bots <TechifyBots@gmail.com>
2026-09-16 15:23:55 +0530

    Update callback.py

--- TechifyBots/callback.py
+++ TechifyBots/callback.py
@@ -1,6 +1,6 @@
 import traceback
 import random
-from pyrogram import Client
+from pyrogram import Client, enums
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from Script import text
 from config import ADMIN, PICS
@@ -19,7 +19,7 @@ async def callback_query_handler(client, query: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton('ℹ️ 𝖠𝖻𝗈𝗎𝗍',callback_data='about'),
                      InlineKeyboardButton('📚 𝖧𝖾𝗅𝗉',callback_data='help')],
-                    [InlineKeyboardButton('⚡ 𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖲𝗍𝗋𝗂𝗇𝗀 𝖲𝖾𝗌𝗌𝗂𝗈𝗇',callback_data='generate')]
+                    [InlineKeyboardButton('⚡ 𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖲𝗍𝗋𝗂𝗇𝗀 𝖲𝖾𝗌𝗌𝗂𝗈𝗇',callback_data='generate', style=enums.ButtonStyle.PRIMARY)]
                 ])
             )
 
@@ -32,7 +32,7 @@ async def callback_query_handler(client, query: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌',url='https://telegram.me/Techifybots'),
                      InlineKeyboardButton('🛟 𝖲𝗎𝗉𝗉𝗈𝗋𝗍',url='https://telegram.me/TechifySupport')],
-                    [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start")]
+                    [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start", style=enums.ButtonStyle.PRIMARY)]
                 ])
             )
 
@@ -45,7 +45,7 @@ async def callback_query_handler(client, query: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton('💻 𝖱𝖾𝗉𝗈',url='https://github.com/TechifyBots/String-Session-Bot'),
                      InlineKeyboardButton('👨‍💻 𝖮𝗐𝗇𝖾𝗋',user_id=int(ADMIN))],
-                    [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start")]
+                    [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄",callback_data="start", style=enums.ButtonStyle.PRIMARY)]
                 ])
             )
 
