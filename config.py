bc2b7bb7c642836111ae791b5b63b7e4c4a41aad
Mamacantik1028 <bulansajay516@gmail.com>
2026-09-17 21:46:04 +0700

    Refactor environment variable loading in config.py

--- config.py
+++ config.py
@@ -1,15 +1,32 @@
 import os
+from dotenv import load_dotenv
 from typing import List
+
+load_dotenv()
 
 API_ID = os.environ.get("API_ID", "")
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-PICS = (os.environ.get("PICS", "https://i.ibb.co/MDssddJp/pic.jpg https://i.ibb.co/n8fQ2xcx/pic.jpg")).split()
-ADMIN = int(os.environ.get("ADMIN", ""))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+PICS = os.environ.get(
+    "PICS",
+    ""
+).split()
+
+ADMIN = int(os.environ.get("ADMIN", "0"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "")
-IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNELS", "").split())) # Add Multiple channel ids
-AUTH_REQ_CHANNELS = list(map(int, os.environ.get("AUTH_REQ_CHANNELS", "").split())) # Add Multiple channel ids
-FSUB_EXPIRE = int(os.environ.get("FSUB_EXPIRE", 2))  # minutes, 0 = no expiry
+
+IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"
+
+AUTH_CHANNELS = list(
+    map(int, os.environ.get("AUTH_CHANNELS", "").split())
+)
+
+AUTH_REQ_CHANNELS = list(
+    map(int, os.environ.get("AUTH_REQ_CHANNELS", "").split())
+)
+
+FSUB_EXPIRE = int(os.environ.get("FSUB_EXPIRE", "2"))
